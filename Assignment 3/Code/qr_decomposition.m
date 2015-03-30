b2f06f182ad27851419a2141f5fcf5cd0fdda10b
metalhead6666 <noe.godinho.92@gmail.com>
2015-03-30 23:46:53 +0100

    Obtain intrinsic and extrinsic values from QR decomposition.

--- Assignment 3/Code/qr_decomposition.m
+++ Assignment 3/Code/qr_decomposition.m
@@ -1,6 +1,37 @@
 function qr_decomposition(M)
-    [Q, R] = qr(flipud(M)');
+    [R, K] = qr(inv(M(1:3, 1:3)));
     
-    R = rot90(R', 2);
-    Q = flipud(Q');
+    if K(1, 1) < 0
+        S = [-1 0 0; 0 1 0; 0 0 1];
+        R = R * S;
+        K = S * K;
+    end
+
+    if K(2, 2) < 0
+        S = [1 0 0; 0 -1 0; 0 0 1];
+        R = R * S;
+        K = S * K;
+    end
+
+    if K(3, 3) < 0
+        S = [1 0 0; 0 1 0; 0 0 -1];
+        R = R * S;
+        K = S * K;
+    end
+    
+    % Translation Vector
+    t = K * M(1:3, 4);
+
+    % if R is a rotation matrix, then det(R) = 1.
+    if det(R) < 0 
+        t = -t;
+        R = -R;
+    end
+
+    % Rotation Matrix
+    R = inv(R);
+
+    % Intrinsic Matrix
+    K = inv(K);
+    K = K / K(3, 3);
 end
