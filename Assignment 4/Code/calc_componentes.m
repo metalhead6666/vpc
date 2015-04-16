78175176b37c89560c865c6ed9cb5894c3a1a846
Christophe235 <christophe_1011@hotmail.com>
2015-04-16 23:14:47 +0100

    Exercise 3 Done! Misson Complete! (y)

--- Assignment 4/Code/calc_componentes.m
+++ Assignment 4/Code/calc_componentes.m
@@ -4,6 +4,7 @@ function[] = calc_componentes()
     
     %exercise 1    
         [image, map] = imread('peppers.png');
+        image = rgb2gray(image);
         imshow(image);
         title('Imagem peppers.png original');
         saveas(gcf, '../Output/original_image', 'jpg');
@@ -11,22 +12,26 @@ function[] = calc_componentes()
     %exercise 2
         %shift de 2 pixels na horizontal
         h_shift_image = circshift(image,[0 2]);
+        imshow(h_shift_image);
         title('Imagem peppers.png com shift horizontal');
         saveas(gcf, '../Output/h_shift_image', 'jpg');
-        imshow(h_shift_image);
 
         %shift de 2 pixels na vertical
         v_shift_image = circshift(image,[2 0]);
+        imshow(v_shift_image);
         title('Imagem peppers.png com shift vertical');
         saveas(gcf, '../Output/v_shift_image', 'jpg');
-        imshow(v_shift_image);
-
+                
         %shift de 2 pixels na diagonal
         d_shift_image = circshift(image,[2 2]);
+        imshow(d_shift_image);
         title('Imagem peppers.png com shift diagonal');
         saveas(gcf, '../Output/d_shift_image', 'jpg');
-        imshow(d_shift_image);
         
+    %exercise 3 
+        calc_fluxo(image, h_shift_image, 'shift horizontal');
+        calc_fluxo(image, v_shift_image, 'shift vertical');
+        calc_fluxo(image, d_shift_image, 'shift diagonal');
         
     close all
     
