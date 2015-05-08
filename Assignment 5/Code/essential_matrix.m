2ab925a5686bd821a77fd277a2ed7c348fc4503d
metalhead6666 <noe.godinho.92@gmail.com>
2015-05-08 18:53:14 +0100

    Essential matrix calculated

--- Assignment 5/Code/essential_matrix.m
+++ Assignment 5/Code/essential_matrix.m
@@ -1,11 +1,10 @@
 function E = essential_matrix(F)
 %% part two
-% parametros intrisecos das cameras
+% intrinsic parameters of the cameras
     Kesq = [1317.24980 0 512.23259; 0 1316.83041 413.07903; 0 0 1];
     Kdir = [1312.94890 0 533.80481; 0 1313.02652 397.59032; 0 0 1];
     
 %% exercise 5
-% determinação da matriz essencial
+% essential matrix determination
     E = Kdir' * F * Kesq
 end
-                    
