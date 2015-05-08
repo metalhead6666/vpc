021a905f73409706ccbd058d84870c9cbb6040e6
metalhead6666 <noe.godinho.92@gmail.com>
2015-05-08 17:18:37 +0100

    Fundamental matrix created

--- Assignment 5/Code/fundamental_matrix.m
+++ Assignment 5/Code/fundamental_matrix.m
@@ -17,16 +17,51 @@ function F = fundamental_matrix()
     showImage(left_image_02, left_image_corners_02, 'LeftCamera02');
     showImage(left_image_05, left_image_corners_05, 'LeftCamera05');
 
-%% exercise one   
-% calculate the fundamental matrix with 8 points algorithm
-% normalização das coordenadas
-pe = [left_image_corners_02(:,1) left_image_corners_02(:,2) left_image_corners_05(:,1) left_image_corners_05(:,2)];
-pe = [pe ones(10,1)];
-pd = [right_image_corners_02(:,1) right_image_corners_02(:,2) right_image_corners_05(:,1) right_image_corners_05(:,2)];
-pd = [pd ones(10,1)];
+    %% exercise one   
+    % calculate the fundamental matrix with 8 points algorithm
+    % normalization of the coordinates
+    pe = [left_image_corners_02(:, 1) left_image_corners_02(:, 2) left_image_corners_05(:, 1) left_image_corners_05(:, 2)];
+    pe = [pe ones(10, 1)];
+    pd = [right_image_corners_02(:, 1) right_image_corners_02(:, 2) right_image_corners_05(:, 1) right_image_corners_05(:, 2)];
+    pd = [pd ones(10, 1)];
 
-
+    xe = mean(pe(:, 1));
+    ye = mean(pe(:, 2));
+    xd = mean(pd(:, 1));
+    yd = mean(pd(:, 2));
     
+    d_e = sum(sqrt((pe(:, 1) - xe) .^ 2 + (pe(:, 2) - ye) .^ 2)) ./ (10 * sqrt(2));
+    d_d = sum(sqrt((pd(:, 1) - xd) .^ 2 + (pd(:, 2) - yd) .^ 2)) ./ (10 * sqrt(2));
+    
+    Te = [1 0 -xe; 1 0 -ye; 0 0 d_e];
+    Td = [1 0 -xd; 1 0 -yd; 0 0 d_d];
+    
+    pe_normalized = Te * pe';
+    pe_normalized = (pe_normalized / 168.9889)';
+    pd_normalized = Td * pd';
+    pd_normalized = (pd_normalized / 108.1601)';
+    
+    A = zeros(length(pe_normalized), 9);
+    
+    for i = 1 : length(pe_normalized)
+        A(i, :) = [pd_normalized(i, 1)*pe_normalized(i, 1) pd_normalized(i, 1)*pe_normalized(i, 2) pd_normalized(i, 1) ...
+                   pd_normalized(i, 2)*pe_normalized(i, 1) pd_normalized(i, 2)*pd_normalized(i, 2) pd_normalized(i, 2) ...
+                   pe_normalized(i, 1) pe_normalized(i, 2) 1];
+    end
+    
+    [~, D, V] = svd(A);
+    
+    [~, index] = min(sum(D));
+    F_normalized = reshape(V(:, index), 3, 3);
+    
+    [U, D, V] = svd(F_normalized);
+    
+    [~, index] = min(sum(D));
+    D(:, index) = 0;
+    
+    F_normalized = U * D * V';
+    
+    F = Td' * F_normalized * Te;
 end
 
 function image = readImage(str)
